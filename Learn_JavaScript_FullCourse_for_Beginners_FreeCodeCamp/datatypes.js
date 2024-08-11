415c1b6761953e4a9163c6453f725c522c3489f2
Tapan Kumar Patra <tapankpatro@gmail.com>
2024-08-11 14:03:55 +0530

    Update datatypes.js

--- Learn_JavaScript_FullCourse_for_Beginners_FreeCodeCamp/datatypes.js
+++ Learn_JavaScript_FullCourse_for_Beginners_FreeCodeCamp/datatypes.js
@@ -1,1 +1,34 @@
+//write to console
+console.log("Hello, World!");
 
+//declare variable
+var number = 5; //in-line comment 
+
+/*  Data types
+
+    undefined
+    null
+    boolean
+    string
+    symbol
+    number
+    objects     */
+
+//variable
+var myName = "Beau";
+myName =8;
+
+let ourName = "freeCodeCamp"; //within scope
+
+const pi = 3.14 //never change
+
+// storing values within assignment operator
+var a;
+var b = 2; a=7; b=a; 
+console.log(a);
+
+// case sensitivity in variable 
+var MYNAME, mYName, myname;
+
+// camelcase in variable name
+var studlyCapVar;
